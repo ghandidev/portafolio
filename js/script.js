c87bdc5497f3fad8ab5cc0bccb9dcfc43af8fefe
ghandidev <ghandiaboultaif@gmail.com>
2024-05-29 03:38:50 -0400

    responsive details

--- js/script.js
+++ js/script.js
@@ -2,20 +2,15 @@ let swiperCards = new Swiper('.card__content', {
   loop: true,
   spaceBetween: 32,
   grabCursor: true,
-
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
     dynamicBullets: true,
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
   breakpoints: {
     600: {
       slidesPerView: 2,

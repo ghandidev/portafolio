ef22d0750b1d6230a7eb07b354d106e5cd9881f9
ghandidev <ghandiaboultaif@gmail.com>
2024-06-08 00:42:45 -0400

    updating responsive

--- js/script.js
+++ js/script.js
@@ -104,3 +104,44 @@ exitBtn.addEventListener('click', () => {
   langBar.classList.add('hide');
   colorBar.classList.add('hide');
 });
+
+function makeEaseOut(timing) {
+  return function (timeFraction) {
+    return 1 - timing(1 - timeFraction);
+  };
+}
+
+function bounce(timeFraction) {
+  for (let a = 0, b = 1; 1; a += b, b /= 2) {
+    if (timeFraction >= (7 - 4 * a) / 11) {
+      return (
+        -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+      );
+    }
+  }
+}
+
+function quad(timeFraction) {
+  return Math.pow(timeFraction, 2);
+}
+
+ball.onclick = function () {
+  let height = field.clientHeight - ball.clientHeight;
+  let width = 250;
+
+  animate({
+    duration: 2000,
+    timing: makeEaseOut(bounce),
+    draw: function (progress) {
+      ball.style.top = height * progress + 'px';
+    },
+  });
+
+  animate({
+    duration: 2000,
+    timing: makeEaseOut(quad),
+    draw: function (progress) {
+      ball.style.left = width * progress + 'px';
+    },
+  });
+};
